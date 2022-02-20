7d2ba22657afaf7ecfdb73dd5b51b538d6f5ef1d
Jonathan Cheng <jcheng461@gmail.com>
2022-02-20 23:01:27 +0900

    add fade and slide elements on two paragraphs below the walrus picture. Use id for these new elements

--- js/scripts.js
+++ js/scripts.js
@@ -3,4 +3,16 @@ $(document).ready(function () {
     $("#walrus-showing").toggle();
     $("#walrus-hidden").toggle();
   });
+
+  // $(".click").click(function () {
+  //   $("#fade").fadeToggle();
+  // });
+
+  $("#clickToFade").click(function () {
+    $("#clickToFadeTarget").fadeToggle();
+  });
+
+  $("#clickToSlide").click(function () {
+    $("#clickToSlideTarget").slideToggle();
+  });
 });
